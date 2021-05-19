110c2c1f366a753c9f8e236021c87b67eef8415a
Ralph114 <simek.r03@htlwienwest.at>
2021-05-19 11:20:13 +0200

    Initial Commit

---
+++ RPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "John";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "Hier lebst du!";
+            CurrentLocation.ImageName = "/Engine/Images/Locations/haus.jpg";
+        }
+    }
+}
